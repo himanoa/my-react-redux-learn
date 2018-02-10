21af219df11e5106146cd8ea09c85b0b037b7208
himanoa <matsunoappy@gmail.com>
2018-02-11 04:43:24 +0900

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const ENV = process.env.NODE_ENV || 'development'
+const DEV_PORT = process.env.PORT || 4444
+
+module.exports = {
+  entry: {
+    app: ['./src/index.js']
+  },
+  output: {
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
+    path: __dirname + '/public',
+    publicPath: '/'
+  },
+  devServer: {
+    contentBase: 'public/',
+    historyApiFallback: true,
+    port: DEV_PORT
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+}
